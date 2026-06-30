0f0437cceadfcd10ed5aecee5cc538e1dac60a2e
To Que Phuong Tran <tran16@ads.uni-passau.de>
2026-06-30 15:51:18 +0200

    Align current code with pycoSHARK 2.0.0

--- plugin_packaging/install.sh
+++ plugin_packaging/install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd ${1}
-python3.5 ${1}/setup.py install --user
+python3 -m pip install --user --no-cache-dir --upgrade --upgrade-strategy only-if-needed "pycoshark @ git+https://github.com/smartshark/pycoSHARK.git@2.0.0"
+python3 ${1}/setup.py install --user
